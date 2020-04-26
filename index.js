c68bd662c4b5a2cfea28dad980686bd5ddbac3c7
Dan Finlay <dan@danfinlay.com>
2020-04-25 17:32:53 -0700

    Added service worker (not working yet)
    Added lots of logs that should fire when it starts working, but
    currently none of those logs trigger.
    
    The service worker currently is set up to intercept any request and
    return `undefined`, which _I hope_ would result in restricting this
    iFrame from making any web requests, but we'll see!

--- index.js
+++ index.js
@@ -9,13 +9,46 @@ body {
 
 `;
 
+const isolating_header = `
+<script>
+console.log('isolating header injected!')
+if ('serviceWorker' in navigator) {
+    console.log('serviceworker is supported, registering')
+    console.dir(navigator.serviceWorker)
+    navigator.serviceWorker.register('./sw.js', {
+        scope: './'
+    }).then(function (registration) {
+      console.log('registered', registration)
+        var serviceWorker;
+        if (registration.installing) {
+            serviceWorker = registration.installing;
+            document.querySelector('#kind').textContent = 'installing';
+        } else if (registration.waiting) {
+            serviceWorker = registration.waiting;
+            document.querySelector('#kind').textContent = 'waiting';
+        } else if (registration.active) {
+            serviceWorker = registration.active;
+            document.querySelector('#kind').textContent = 'active';
+        }
+        if (serviceWorker) {
+            // logState(serviceWorker.state);
+            serviceWorker.addEventListener('statechange', function (e) {
+                // logState(e.target.state);
+            });
+        }
+    }).catch (function (error) {
+        // Something went wrong during registration. The service-worker.js file
+        // might be unavailable or contain a syntax error.
+    });
+} else {
+  console.log('sw not supported')
+}
+</script>
+`
+
 window.addEventListener('load', ready)
 
 function ready () {
-  injectIframe(malicious_html);
-}
-
-function injectIframe (html) {
     var iframe = document.createElement("iframe");
     var img = document.createElement('img');
     img.src = 'http://www.fillmurray.com/200/300'
@@ -23,9 +56,8 @@ function injectIframe (html) {
     container.appendChild(iframe);
     iframe.name = "frame"
     iframe.id = 'test-iframe';
-    console.dir(iframe.document)
-    console.dir(img)
-    injectHTML(iframe, html)
+    injectHTML(iframe, isolating_header)
+    injectHTML(iframe, malicious_html)
 }
 
 // Modified from:
@@ -53,3 +85,4 @@ function injectHTML(iframe, html_string){
      }
 
 }
+
